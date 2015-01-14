e7756f9947dd49b43d59b5c44b0a005d0cdf9c01
siffer <gichan@126.com>
2015-01-14 22:45:35 +0800

    package number Class First

---
+++ src/Part3/Number/First.java
@@ -0,0 +1,10 @@
+package Part3.Number;
+
+public class First {
+	static String s1 = "nihao";
+	public static void main(String[] args){
+		String s2 = "Java";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
